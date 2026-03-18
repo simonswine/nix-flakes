c88a5b8f0599af558808344dbb0bfab9b13c4ccd
Christian Simon <simon@swine.de>
2026-03-18 09:26:21 +0000

    Update dot-net environment

--- pyroscope-dotnet/flake.nix
+++ pyroscope-dotnet/flake.nix
@@ -3,36 +3,34 @@
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
   };
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem
-      (system:
-        let
-          overlays = [ ];
-          pkgs = import nixpkgs {
-            inherit system overlays;
-            config.allowUnfree = true;
-          };
-          myvscode = pkgs.vscode-with-extensions.override {
-            vscodeExtensions = with pkgs.vscode-extensions; [
-              ms-dotnettools.csharp
-              ms-dotnettools.csdevkit
-            ];
-          };
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        overlays = [ ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+          config.allowUnfree = true;
+        };
+      in
+      with pkgs;
+      {
+        devShells.default = mkShell {
+          # General stuff
+          name = "pyroscope-dotnet";
+          nativeBuildInputs = [
+            dotnet-sdk
+            cmake
+            openssl
+            pkg-config
+          ];
+        };
 
-        in
-        with pkgs;
-        {
-          devShells.default =
-            mkShell
-              {
-                # General stuff
-                name = "pyroscope-dotnet";
-                nativeBuildInputs = [
-                  myvscode
-                  dotnet-sdk
-                ];
-              };
-
-        }
-      );
+      }
+    );
 }
