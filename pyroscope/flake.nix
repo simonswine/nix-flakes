f561df26a49dc1679f024c9ba4698b478160cea7
Christian Simon <simon@swine.de>
2026-04-02 17:02:05 +0100

    Add pyroscope

--- pyroscope/flake.nix
+++ pyroscope/flake.nix
@@ -3,79 +3,88 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
     flake-utils.url = "github:numtide/flake-utils";
   };
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem
-      (system:
-        let
-          overlays = [
-            (
-              self: super: {
-                yarn = super.yarn.override {
-                  nodejs = pkgs.nodejs_22;
-                };
-              }
-            )
-          ];
-          pkgs = import nixpkgs {
-            inherit system overlays;
-          };
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        overlays = [
+          (self: super: {
+            yarn = super.yarn.override {
+              nodejs = pkgs.nodejs_22;
+            };
+          })
+        ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
 
-          recursiveMerge =
-            with nixpkgs.lib;
-            attrList:
-            let
-              f = attrPath:
-                zipAttrsWith (n: values:
-                  if tail values == [ ]
-                  then head values
-                  else if all isList values
-                  then unique (concatLists values)
-                  else if all isAttrs values
-                  then f (attrPath ++ [ n ]) values
-                  else last values
-                );
-            in
-            f [ ] attrList;
+        recursiveMerge =
+          with nixpkgs.lib;
+          attrList:
+          let
+            f =
+              attrPath:
+              zipAttrsWith (
+                n: values:
+                if tail values == [ ] then
+                  head values
+                else if all isList values then
+                  unique (concatLists values)
+                else if all isAttrs values then
+                  f (attrPath ++ [ n ]) values
+                else
+                  last values
+              );
+          in
+          f [ ] attrList;
 
-        in
-        with pkgs;
-        {
-          devShells.default =
-            mkShell (recursiveMerge [
-              {
-                # General stuff
-                name = "pyroscope";
+      in
+      with pkgs;
+      {
+        devShells.default = mkShell (recursiveMerge [
+          {
+            # General stuff
+            name = "pyroscope";
 
-                nativeBuildInputs = [
-                  buf
-                  pkg-config
-                ];
+            nativeBuildInputs = [
+              buf
+              pkg-config
+            ];
 
-                buildInputs = [
-                  go_1_24
-                ];
+            buildInputs = [
+              go_1_26
+            ];
 
-              }
-              {
-                # Frontend stuff
-                nativeBuildInputs = [ yarn nodejs_20 wget ];
-                buildInputs = [
-                  pixman
-                  cairo
-                  libpng
-                  pango
-                  glib.dev
-                  harfbuzz.dev
-                  freetype.dev
-                  python3
-                ];
-              }
-              {
-                # Examples rust stuff
-                buildInputs = [ libiconv ];
-              }
-            ]);
+          }
+          {
+            # Frontend stuff
+            nativeBuildInputs = [
+              yarn
+              nodejs_20
+              wget
+            ];
+            buildInputs = [
+              pixman
+              cairo
+              libpng
+              pango
+              glib.dev
+              harfbuzz.dev
+              freetype.dev
+              python3
+            ];
+          }
+          {
+            # Examples rust stuff
+            buildInputs = [ libiconv ];
+          }
+        ]);
 
-        }
-      );
+      }
+    );
 }
